aba3474e91efc30d856a1efad15ce237211f74aa
David Whatley <nsxdavid@gmail.com>
2025-05-06 02:15:55 -0500

    chore: Fixed the folder structure to be way more standard

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { SurrealDb } from '../nodes/SurrealDb/SurrealDb.node';
-import { SurrealDbApi } from '../credentials/SurrealDbApi.credentials';
+import { SurrealDb } from './nodes/SurrealDb/SurrealDb.node';
+import { SurrealDbApi } from './credentials/SurrealDbApi.credentials';
 
 export const nodeTypes = [
 	SurrealDb,
